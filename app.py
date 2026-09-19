8e6580f2c02c7d8a68ddf6590d524a23df235674
atharv3434 <atharvanakti008@gmail.com>
2026-09-19 22:35:17 +0530

    Initial Project is Done

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "")
 APP_ENV = os.getenv("APP_ENV", "production")
 
-# Redis connection
+
 client = redis.Redis(
     host=REDIS_HOST,
     port=REDIS_PORT,
